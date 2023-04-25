61e217619b2161ad81f58ecd14582c8ec70b310d
Emily Pessoa <emymi2007@gmail.com>
2023-04-25 07:14:18 -0300

    Alterei o css, adicionei o estados e arrumei o js.

---
+++ script.js
@@ -0,0 +1,18 @@
+function Verificar(){
+    let nome = document.getElementById('nome').value;
+    let telefone = document.getElementById('telefone').value;
+    let nascimento = document.getElementById('nascimento').value;
+    let cpf = document.getElementById('cpf').value;
+    let endereco = document.getElementById('endereco').value;
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    let csenha = document.getElementById('csenha').value;
+    let sobrenome = document.getElementById('csenha').value;
+    let numero = document.getElementById('numero').value;
+    let cidadenome = document.getElementById('cidadenome').value;
+
+    if(!email || !senha || !nome || !telefone || !nascimento || !cpf || !endereco || !csenha || !sobrenome || !numero || !cidadenome)
+    alert("Campos de preenchimento obrigatório. Favor preencher.")
+    
+    
+}
